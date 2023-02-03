2e9d1d5427845241d6d9d302c776e180d954d60c
AndrianaViryovka <andriana99@gmail.com>
2023-02-04 00:53:26 +0300

    Default city was changed

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
         <div className="container">
           <header className="App-header">
             <p>
-              <Weather defaultCity="Kyiv" />
+              <Weather defaultCity="Sydney" />
               <small class="GitNetlifyLink">
                 <a
                   id="git-link"
