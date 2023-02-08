d6aaa20a6e46953e444f67b0bc34f26cf2101913
AndrianaViryovka <andriana99@gmail.com>
2023-02-08 18:58:31 +0300

    Fixed UI

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -10,43 +10,48 @@ export default function WeatherInfo(props) {
       <div class="container text-right">
         <div class="row">
           <div class="col-6">
-            <h1 id="city">{props.data.city}</h1>
-            <p class="dateMonth" id="datetime">
-              <FormattedDate date={props.data.date} />
-            </p>
+            <h1 id="city">
+              {props.data.city}{" "}
+              <img
+                alt={props.data.description}
+                id="icon"
+                src={props.data.iconUrl}
+              />
+            </h1>
+
             <div class="temperature">
               <span id="description">{props.data.description}</span>
             </div>
+            <p class="dateMonth" id="datetime">
+              <FormattedDate date={props.data.date} />
+            </p>
           </div>
 
           <div class="col-6">
             <div class="row">
-              <div class="col-4">
-                <img
-                  alt={props.data.description}
-                  id="icon"
-                  src={props.data.iconUrl}
-                />
-              </div>
-              <div class="col-8">
+              <div class="col-12">
                 <WeatherTemperature celsius={props.data.temperature} />
               </div>
             </div>
-            <ul>
-              <li>
-                Feels like:{" "}
-                <span id="feels_like">{Math.round(props.data.feels_like)}</span>
-                °C
-              </li>
-              <li>
-                Humidity:{" "}
-                <span id="humidity">{Math.round(props.data.humidity)}</span>%
-              </li>
-              <li>
-                Wind: <span id="wind">{Math.round(props.data.wind)}</span>
-                km/h
-              </li>
-            </ul>
+            <p>
+              <ul>
+                <li>
+                  Feels like:{" "}
+                  <span id="feels_like">
+                    {Math.round(props.data.feels_like)}
+                  </span>
+                  °C
+                </li>
+                <li>
+                  Humidity:{" "}
+                  <span id="humidity">{Math.round(props.data.humidity)}</span>%
+                </li>
+                <li>
+                  Wind: <span id="wind">{Math.round(props.data.wind)}</span>
+                  km/h
+                </li>
+              </ul>
+            </p>
           </div>
         </div>
       </div>
